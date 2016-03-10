ec0fb772ab1ae7079cce148cb4a812e385adedd3
revers <revers@yandex-team.ru>
2016-03-10 04:29:12 +0300

    more ugly code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 var loaderUtils = require('loader-utils');
+var fs = require('fs');
 
 var yate = require('yate');
 
 module.exports = function() {
     this.cacheable();
+    var file = loaderUtils.getRemainingRequest(this);
+    var compiled = yate.compile(file).js;
+    fs.writeFileSync(file.split('m-t-').join('t-') + '.min.js', compiled);
     return yate.compile(loaderUtils.getRemainingRequest(this)).js;
 };
